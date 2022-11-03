81d7c3990aa981d6b129ff26e4ec13414d17b01f
evolshenanigans <gjgutierrez91@gmail.com>
2022-11-03 10:04:11 -0700

    modified:   src/App.js 	new file:   src/assets/shopping-bag.svg 	new file:   src/components/cart-dropdown/cart-dropdown.component.jsx 	new file:   src/components/cart-dropdown/cart-dropdown.styles.scss 	new file:   src/components/cart-icon/cart-icon.component.jsx 	new file:   src/components/cart-icon/cart-icon.styles.scss 	new file:   src/components/product-card/product-card.component.jsx 	new file:   src/components/product-card/product-card.styles.scss 	new file:   src/contexts/cart.context.jsx 	new file:   src/contexts/products.context.jsx 	modified:   src/index.js 	modified:   src/index.scss 	modified:   src/routes/navigation/navigation.component.jsx 	new file:   src/routes/shop/shop.component.jsx 	new file:   src/routes/shop/shop.styles.scss 	new file:   src/shop-data.json

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,10 +3,14 @@ import { Fragment, useContext } from "react";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import "./navigation.styles.scss";
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
 import {signOutUser} from "../../utils/firebase/firebase.utils";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
@@ -25,7 +29,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown/>}
       </div>
       <Outlet />
     </Fragment>
