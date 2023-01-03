d1f61a7647a77ff7bf27aee7d50eed80fa914a26
evolshenanigans <gjgutierrez91@gmail.com>
2023-01-03 10:31:40 -0800

    modified:   src/App.js 	modified:   src/components/cart-dropdown/cart-dropdown.component.jsx 	new file:   src/components/checkout-item/checkout-item.component.jsx 	new file:   src/components/checkout-item/checkout-item.styles.scss 	modified:   src/components/product-card/product-card.component.jsx 	modified:   src/contexts/cart.context.jsx 	new file:   src/routes/checkout/checkout.component.jsx 	new file:   src/routes/checkout/checkout.styles.scss

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -14,11 +14,26 @@ const addCartItem = (cartItems, productToAdd) => {
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
+const removeCartItem = (cartItems, cartItemToRemove) => {
+  const exisitingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+  if (exisitingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
+  removeItemFromCart: () => {},
   cartCount: 0
 });
 
@@ -35,7 +50,10 @@ export const CartProvider = ({ children }) => {
   const addItemToCart = (productToAdd) => {
     setCartItems(addCartItem(cartItems, productToAdd));
   };
+  const removeItemToCart = (cartItemToRemove) => {
+    setCartItems(removeCartItem(cartItems, cartItemToRemove));
+  };
 
-  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount };
+  const value = { isCartOpen, setIsCartOpen, removeItemToCart, addItemToCart, cartItems, cartCount };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,36 @@
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import "./checkout.styles.scss";
+
+const Checkout = () => {
+  const { cartItems } =
+    useContext(CartContext);
+  return (
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>product</span>
+        </div>
+        <div className="header-block">
+          <span>description</span>
+        </div>
+        <div className="header-block">
+          <span>quantity</span>
+        </div>
+        <div className="header-block">
+          <span>price</span>
+        </div>
+        <div className="header-block">
+          <span>remove</span>
+        </div>
+      </div>
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
+      ))}
+      <span className="Total">total:0</span>
+    </div>
+  );
+};
+
+export default Checkout;
