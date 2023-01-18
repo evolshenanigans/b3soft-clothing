ab4beafac37c7c959186ddba95bab2479f5a99f9
evolshenanigans <gjgutierrez91@gmail.com>
2023-01-17 19:01:24 -0800

    added styled components cart-dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,7 +5,7 @@ import { CartContext } from '../../contexts/cart.context';
 
 
 import Button from '../button/button.component';
-import './cart-dropdown.styles.scss';
+import {CartDropdownContainer, EmptyMessage, CartItems} from './cart-dropdown.styles';
 import CartItem from '../cart-item/cart-item.component'
 
 const CartDropdown = () => {
@@ -17,14 +17,17 @@ const CartDropdown = () => {
     }
     
     return (
-        <div className='cart-dropdown-container'>
-            <div className='cart-items'>
-                {cartItems.map((item) => (
-                    <CartItem key={item.id} cartItem={item} />
-                ))}
-            </div>
+        <CartDropdownContainer>
+            <CartItems>
+                {
+                    cartItems.length ? (cartItems.map((item) => (
+                        <CartItem key={item.id} cartItem={item} />
+                    ))) : (<EmptyMessage>Your cart is empty</EmptyMessage>)
+                }
+        
+            </CartItems>
             <Button onClick={gotoCheckoutHandler}> go to checkout </Button>
-        </div>
+        </CartDropdownContainer>
     );
 }
 
--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
 import FormInput from "../form-input/form-input.component";
 import {
-  createAuthUserWithEmailAndPassword,
-  createUserDocumentFromAuth,
   signInAuthUserWithEmailAndPassword,
   signInWithGooglePopup,
 } from "../../utils/firebase/firebase.utils";
 
 import "./sign-in-form.styles.scss";
-import Button from "../button/button.component";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 
 const defaultFormFields = {
   email: "",
@@ -76,7 +74,11 @@ const SignInForm = () => {
         />
         <div className="buttons-container">
           <Button type="submit">Sign In</Button>
-          <Button type="button" buttonType="google" onClick={signInWithGoogle}>
+          <Button
+            buttonType={BUTTON_TYPE_CLASSES.google}
+            type="button"
+            onClick={signInWithGoogle}
+          >
             Google Sign In
           </Button>
         </div>
